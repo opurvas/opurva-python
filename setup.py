ae914a7aa0459e5024c1ed0107d9c174c1e5f535
imrkgofficial <imrkaofficial@gmail.com>
2025-05-14 18:50:56 +0530

    setup commit

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-import setuptools 
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
-setuptools.setup(name='opurva',
-      version='1.0.0',
-      description='sdk for opurva API client and TwiML generator',
-      author='opurva',
-      author_email='therajatkumargoyal@gmail.com',
-      url='https://github.com/opurvas/opurva-python.git',
-      install_requires=['twilio==9.6.1'],
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      packages=setuptools.find_packages(),
-      include_package_data=True,
-      classifiers=[
+
+setuptools.setup(
+    name='opurva',
+    version='1.0.0',
+    description='SDK for Opurva API client and TwiML generator',
+    author='opurva',
+    author_email='therajatkumargoyal@gmail.com',
+    url='https://github.com/opurvas/opurva-python.git',
+    install_requires=['twilio==9.6.1'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
@@ -29,5 +30,5 @@ setuptools.setup(name='opurva',
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Communications :: Telephony",
     ],
-      python_requires=">=3.7.0"
+    python_requires=">=3.7.0"
 )
